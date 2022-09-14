0c6744ffc06de146ddc298bb80e38fc2e60d34dc
bookland <rratcliffe57@gmail.com>
2022-09-14 12:59:47 -0500

    add antd

--- pages/index.js
+++ pages/index.js
@@ -1,8 +1,9 @@
 import Head from 'next/head'
 import Image from 'next/image'
+import { DatePicker } from 'antd';
+
 
 export default function Home() {
   return (
-    "hi"
-  )
+    <DatePicker />)
 }
